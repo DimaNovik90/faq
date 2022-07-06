498a2fbf3489542d577d9501f1d2fc303e5ff698
Novik Dmitry <vanilla-cvety-krd@yandex.ru>
2022-07-06 11:19:28 +0300

    готовый адаптив

--- scripts/script.js
+++ scripts/script.js
@@ -2,25 +2,21 @@ const butonActiv = document.querySelectorAll('.questions__contaiter-button')
 const form = document.querySelector('.form');
 const searchInpyt = form.querySelector('.form__input-search');
 const btnSearch = form.querySelector('.form__button')
-
-
+const button = document.querySelector('.questions__contaiter-button');
 
 function handlerSerch(evt) {
   evt.preventDefault();
 }
 form.addEventListener('submit', handlerSerch)
 
-
-
-
-
-
-
 butonActiv.forEach((item) => {
   item.addEventListener('click', function(evt) {
     evt.currentTarget.classList.contains('.questions__widget');
-    evt.target.closest('.questions__widget').classList.toggle('questions__widget_visible_text').transition = "2s";
+    if (evt.target.closest('.questions__widget').classList.toggle('questions__widget_visible_text')) {
+      evt.target.src = "../icons/minus.svg"
+    } else {
+      evt.target.src = "../icons/+.svg"
+    }
   })
-})
-
+});
 
